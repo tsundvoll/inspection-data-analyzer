83d05845547925b861d40976e240dd6810b729f7
Thomas Sundvoll <thomas@sundvoll.net>
2024-08-16 10:04:46 +0200

    Change to User Authentication

--- functions/LocalFunctionProj/HttpExample.cs
+++ functions/LocalFunctionProj/HttpExample.cs
@@ -15,7 +15,7 @@ namespace LocalFunctionProj
         }
 
         [Function("HttpExample")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.User, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
             return new OkObjectResult("Welcome to Azure Functions!");
